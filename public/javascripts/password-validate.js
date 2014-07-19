c50c8df2ba0098ae66ec625bf74a4734fa323e27
Timothy O'Hare <timohare@gmail.com>
2014-07-19 21:16:52 +1000

    Add Country selection from Smashing Magazine

--- public/javascripts/password-validate.js
+++ public/javascripts/password-validate.js
@@ -8,68 +8,68 @@
    }
 }
 
-        var x = document.getElementById("reverseGeolocation");
-        var myAddress =  document.getElementById("myAddress");                      
-        function getLocation()
-          {
-            if (navigator.geolocation)
-            {
-              navigator.geolocation.getCurrentPosition(showPosition,showError);
-            }
-          else{x.innerHTML="Geolocation is not supported by this browser.";}
-          }
+var x = document.getElementById("reverseGeolocation");
+var myAddress =  document.getElementById("myAddress");                      
+function getLocation()
+  {
+    if (navigator.geolocation)
+    {
+      navigator.geolocation.getCurrentPosition(showPosition,showError);
+    }
+  else{x.innerHTML="Geolocation is not supported by this browser.";}
+  }
 
-          function showPosition(position)
-          {
-            var latlon=position.coords.latitude+","+position.coords.longitude;
+  function showPosition(position)
+  {
+    var latlon=position.coords.latitude+","+position.coords.longitude;
 
-            var img_url="http://maps.googleapis.com/maps/api/staticmap?center="
-          +latlon+"&zoom=14&size=400x300&sensor=false";
-            document.getElementById("mapholder").innerHTML="<img src='"+img_url+"' />";
+    var img_url="http://maps.googleapis.com/maps/api/staticmap?center="
+  +latlon+"&zoom=14&size=400x300&sensor=false";
+    document.getElementById("mapholder").innerHTML="<img src='"+img_url+"' />";
 
-            var geocoder = new google.maps.Geocoder();
-            var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
-            geocoder.geocode({'latLng': latlng}, function(results, status) {
-              if (status == google.maps.GeocoderStatus.OK) {
-                if (results[0]) {
-                  //map.setZoom(11);
-                  //marker = new google.maps.Marker({
-                  //    position: latlng,
-                  //    map: map
-                  //});
-                  //infowindow.setContent(results[1].formatted_address);
-                  //infowindow.open(map, marker);
-                  
-                  // Display address as text in the page
-                  myAddress.innerHTML="Adress: " + results[0].formatted_address;
-                  console.log("Adress: " + results[0].formatted_address)
-                } else {
-                  console.log('No results found')
-                  alert('No results found');
-                }
-              } else {
-                console.log('Geocoder failed due to: ' + status)
-                alert('Geocoder failed due to: ' + status);
-              }
-            });
-          }
+    var geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+    geocoder.geocode({'latLng': latlng}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        if (results[0]) {
+          //map.setZoom(11);
+          //marker = new google.maps.Marker({
+          //    position: latlng,
+          //    map: map
+          //});
+          //infowindow.setContent(results[1].formatted_address);
+          //infowindow.open(map, marker);
+          
+          // Display address as text in the page
+          myAddress.innerHTML="Address: " + results[0].formatted_address;
+          console.log("Address: " + results[0].formatted_address);
+        } else {
+          console.log('No results found');
+          alert('No results found');
+        }
+      } else {
+        console.log('Geocoder failed due to: ' + status);
+        alert('Geocoder failed due to: ' + status);
+      }
+    });
+  }
 
-        function showError(error)
-          {
-          switch(error.code) 
-            {
-              case error.PERMISSION_DENIED:
-                x.innerHTML="User denied the request for Geolocation."
-                break;
-              case error.POSITION_UNAVAILABLE:
-                x.innerHTML="Location information is unavailable."
-              break;
-              case error.TIMEOUT:
-                x.innerHTML="The request to get user location timed out."
-              break;
-              case error.UNKNOWN_ERROR:
-                x.innerHTML="An unknown error occurred."
-              break;
-            }
-          }
+function showError(error)
+  {
+  switch(error.code) 
+    {
+      case error.PERMISSION_DENIED:
+        x.innerHTML="User denied the request for Geolocation.";
+        break;
+      case error.POSITION_UNAVAILABLE:
+        x.innerHTML="Location information is unavailable.";
+      break;
+      case error.TIMEOUT:
+        x.innerHTML="The request to get user location timed out.";
+      break;
+      case error.UNKNOWN_ERROR:
+        x.innerHTML="An unknown error occurred.";
+      break;
+    }
+  }
 
