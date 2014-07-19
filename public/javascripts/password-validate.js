d016a6ad3944b9f5f8e1d6ac6f81bd8f5db8e102
Timothy O'Hare <timohare@gmail.com>
2014-07-19 22:31:38 +1000

    Prefill form with values from georegister

--- public/javascripts/password-validate.js
+++ public/javascripts/password-validate.js
@@ -11,13 +11,16 @@
 var x = document.getElementById("reverseGeolocation");
 var myAddress =  document.getElementById("myAddress");                      
 function getLocation()
-  {
+{
     if (navigator.geolocation)
     {
       navigator.geolocation.getCurrentPosition(showPosition,showError);
     }
-  else{x.innerHTML="Geolocation is not supported by this browser.";}
-  }
+  else
+    {
+        x.innerHTML="Geolocation is not supported by this browser.";
+    }
+}
 
   function showPosition(position)
   {
@@ -40,9 +43,35 @@ function getLocation()
           //infowindow.setContent(results[1].formatted_address);
           //infowindow.open(map, marker);
           
-          // Display address as text in the page
+          // Display address as text in the page NOTE: This only works in Chrome, not in Firefox
           myAddress.innerHTML="Address: " + results[0].formatted_address;
           console.log("Address: " + results[0].formatted_address);
+          //console.log("Address: " + results[0].address_components[0].long_name);
+          //console.log("Address: " + results[0].address_components[0].long_name);
+
+          // from http://stackoverflow.com/questions/8082405/parsing-address-components-in-google-maps-upon-autocomplete-select
+          var address_components = results[0].address_components;
+          var components={}; 
+          jQuery.each(address_components, function(k,v1) {jQuery.each(v1.types, function(k2, v2){components[v2]=v1.long_name});})
+
+          // print out the returned components
+          jQuery.each(components, function(k,v) { console.log(k + " " + v); });
+
+          // prefill in the forms with the address returned
+          var addressStreet =  document.getElementById("address_street");
+          addressStreet.value = components.street_number + " " + components.route;
+
+          var addressCity = document.getElementById("address_city");
+          addressCity.value = String(components.locality);
+
+          var addressState = document.getElementById("address_state");
+          addressState.value = String(components.administrative_area_level_1);
+
+          var addressCountry = document.getElementById("country-selector");
+          addressCountry.value = String(components.country); 
+
+          var addressPostcode = document.getElementById("address_postcode");
+          addressPostcode.value = String(components.postal_code);
         } else {
           console.log('No results found');
           alert('No results found');
@@ -52,12 +81,12 @@ function getLocation()
         alert('Geocoder failed due to: ' + status);
       }
     });
-  }
+}
 
 function showError(error)
-  {
+{
   switch(error.code) 
-    {
+  {
       case error.PERMISSION_DENIED:
         x.innerHTML="User denied the request for Geolocation.";
         break;
@@ -70,6 +99,6 @@ function showError(error)
       case error.UNKNOWN_ERROR:
         x.innerHTML="An unknown error occurred.";
       break;
-    }
   }
+}
 
